a1e835d4000ca37deb0fb92820d312a548a7b869
rogowoi <mykyta@gotolstoy.com>
2026-02-10 22:26:13 +0100

    fix: Update Stripe API version to 2026-01-28.clover

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -23,7 +23,7 @@ if (missingVars.length > 0) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-02-24.acacia",
+  apiVersion: "2026-01-28.clover",
 });
 
 export const PLANS = {
